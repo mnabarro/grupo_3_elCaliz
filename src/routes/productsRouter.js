72d1ef858e51770c0eaf355d59b534fa8c5cd087
mnabarro <matiasnabarro@gmail.com>
2021-12-13 13:38:12 -0300

    Product edit updated. Not finished.

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,14 +3,20 @@ const router = express.Router();
 
 const productsController = require('../controllers/productsController.js');
 
-router.get('/', productsController.index);
+router.get('/', productsController.index); //Listar todos los productos
 router.get('/category/:category', productsController.category);
-router.get('/search/:criterio', productsController.search);
-router.get('/detail/:id', productsController.productDetail);
-router.get('/edit/:id', productsController.editProduct);
+
+router.get('/search/:criterio', productsController.search); //Ruta aun no solicitada, pensada para la caja de búsqueda.
+
+router.get('/detail/:id', productsController.productDetail); //Detalles de un producto
+
+router.get('/edit/:id', productsController.editProduct); //Muestro el formulario para edición, con los datos del producto cargados
+router.put('/', productsController.editProduct); //Envío los datos para almacenar los cambios de la edición
+
 router.get('/delete/:id', productsController.deleteProduct);
-router.get('/create', productsController.createProduct);
-router.post('/', productsController.newProduct);
+
+router.get('/create', productsController.createProduct); //Muestro el formulario de creación de producto
+router.post('/', productsController.newProduct); //Enviamos los datos para la creación de un nuevo item
 
 
 
