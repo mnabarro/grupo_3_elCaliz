12a69d7bd69222b246f9d3a3e5856d7f0cf54a0f
Marian-pk <maruxs@gmail.com>
2021-12-12 21:58:45 -0300

    CRUD part 2

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -41,15 +41,6 @@ const productsController = {
     index: (req, res) => {res.send( 'Lista todos los productos?');},
     category: (req, res) => {res.render('products/category', {products:products, cssa : 'category.css', title :'Categorías'});},
     search: (req, res) => {res.send(`Productos que coinciden con el criterio :${req.params.criterio}`);},
-  
-    addProductForm: (req, res) => {
-        res.render('products/addProduct', {cssa: 'products-add.css', "categories":categories, title:"Agregar producto"});
-    },
-
-    addProduct: (req, res) => {
-        res.send(req.body);
-    },
-    
     productDetail: (req, res) => {
                 res.render('products/productDetail', {cssa: 'product-detail.css', title:"Detalle del producto"});
     },
@@ -57,8 +48,16 @@ const productsController = {
     editProduct: (req, res) => {
         res.render('products/editProduct', {cssa: 'products-edit.css', "categories":categories, title:"Editar producto"});
     },
-    deleteProduct: (req, res) => {res.send(`Eliminar producto :${req.params.id}`);},
+    deleteProduct: (req, res) => {res.send(`Eliminar producto :${req.params.id}`);
+    },
+    createProduct: (req, res) => {
+        res.render('products/createProduct', {css: 'products-add.css', "categories":categories, title:"Crear un nuevo producto"});
+    },
 
+    newProduct: (req, res) => {
+        res.send(req.body);
+    },
+    
 };
 
 module.exports = productsController;
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -6,11 +6,13 @@ const productsController = require('../controllers/productsController.js');
 router.get('/', productsController.index);
 router.get('/category/:category', productsController.category);
 router.get('/search/:criterio', productsController.search);
-router.get('/Detail/:id', productsController.productDetail);
-router.get('/add', productsController.addProductForm);
-router.post('/', productsController.addProduct);
+router.get('/detail/:id', productsController.productDetail);
 router.get('/edit/:id', productsController.editProduct);
 router.get('/delete/:id', productsController.deleteProduct);
+router.get('/create', productsController.createProduct);
+router.post('/', productsController.newProduct);
+
+
 
 
 module.exports = router;
