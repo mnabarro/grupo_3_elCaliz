405d5cfe7cbef4389fabae1a0e407d85dbdf8d58
Matias <matiasnabarro@gmail.com>
2021-12-26 20:27:56 -0300

    Categories moved to JSON. CSS fixes

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -6,38 +6,41 @@ const { runInNewContext } = require('vm');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+const categoriesFilePath = path.join(__dirname, '../data/categories.json');
+let categories = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
+
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); 
 
-const categories = [
-    {
-    label: 'Blancos',
-    name: 'cat-blancos'
-    },  
-    {
-    label: 'Tintos',
-    name: 'cat-tintos'
-    }, 
-    {
-    label: 'Espumantes',
-    name: 'cat-espumantes'
-    }, 
-    {
-    label: 'C. Tardía',
-    name: 'cat-tardia'
-    }, 
-    {
-    label: 'Especiales',
-    name: 'cat-especiales'
-    }, 
-    {
-    label: 'Accesorios',
-    name: 'cat-accesorios'
-    }, 
-    {
-    label: 'Ofertas',
-    name: 'cat-ofertas'
-    }, 
-];
+// const categories = [
+//     {
+//     label: 'Blancos',
+//     name: 'cat-blancos'
+//     },  
+//     {
+//     label: 'Tintos',
+//     name: 'cat-tintos'
+//     }, 
+//     {
+//     label: 'Espumantes',
+//     name: 'cat-espumantes'
+//     }, 
+//     {
+//     label: 'C. Tardía',
+//     name: 'cat-tardia'
+//     }, 
+//     {
+//     label: 'Especiales',
+//     name: 'cat-especiales'
+//     }, 
+//     {
+//     label: 'Accesorios',
+//     name: 'cat-accesorios'
+//     }, 
+//     {
+//     label: 'Ofertas',
+//     name: 'cat-ofertas'
+//     }, 
+// ];
 
 const adminController = {
     index: (req, res) => {res.render('products/category', {products:products, cssa : 'category.css', title :'Categorías'});},
