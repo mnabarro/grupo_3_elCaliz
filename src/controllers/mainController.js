08725e4949bff6f91219929e5d197374c216802c
gondecu <gon.dc98@gmail.com>
2022-01-11 18:09:46 -0300

    update home destacados ejs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); 
+
+
 const mainController = {
 
     index: (req, res) => {
-        res.render( 'index', {cssa: 'index.css', title:"El Cáliz - Home"});
+        let id = req.params.id
+        const result = products.filter(product => product.id <= 4);
+        res.render( 'index', {result, cssa: 'index.css', title:"El Cáliz - Home"});
     },
 
     regret: (req, res) => {res.send( 'Arrepentimiento');},
