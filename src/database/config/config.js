c5afb4501b1a2944b2434775dfdb329dab63e564
Marian-pk <maruxs@gmail.com>
2022-02-05 17:09:33 -0300

    sequelize configuration

---
+++ src/database/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    "development": {
+      "username": "root",
+      "password": "Digital123",
+      "database": "elcaliz_db",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "operatorAliases": false
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+}
+
