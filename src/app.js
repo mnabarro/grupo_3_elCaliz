656faea32b7f12c397026bee7078a7a43075c27f
Marian-pk <maruxs@gmail.com>
2022-03-13 17:10:50 -0300

    heroku

--- src/app.js
+++ src/app.js
@@ -44,9 +44,13 @@ app.set('view engine', 'ejs');
 
 
 // ************ Port ************
-app.listen(3000, () => {
+app.listen(process.env.PORT) || 3000, function () {
+    console.log("Server listening on port 3000");
+}
+
+/*app.listen(3000, () => {
     console.log('Server listening on port 3000');
-});
+});*/
 
 
 /*
