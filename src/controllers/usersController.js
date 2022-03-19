57c2046ca9be26adfbcf4dc608b83160c8fdf46f
gondecu <gon.dc98@gmail.com>
2022-03-19 15:16:29 -0300

    .

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -134,7 +134,7 @@ const usersController = {
                     });
 
 
-                    return res.redirect('users/login');
+                    return res.redirect('/users/login');
                 }
             }).catch((error) => {
                 if (error) throw error;
