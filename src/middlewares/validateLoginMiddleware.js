dae2bee79832eaf8a91cba23350c731fd2836d77
gondecu <gon.dc98@gmail.com>
2022-03-20 12:32:57 -0300

    .

--- src/middlewares/validateLoginMiddleware.js
+++ src/middlewares/validateLoginMiddleware.js
@@ -3,5 +3,5 @@ const { body } = require('express-validator');
 
 module.exports = [
     body('email').notEmpty().withMessage('Ecribí tu Email').bail().isEmail().withMessage('Tiene que ser un formato de email válido'),
-    body('password').notEmpty().isLength({max:8}).withMessage('Ecribí tu contraseña de maximo 8 caracteres'),
+    body('password').notEmpty().withMessage('Ecribe tu contraseña'),
 ];
