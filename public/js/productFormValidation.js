1cd1fc956de54087ecf2a98ace060d00ed40cbc3
gondecu <gon.dc98@gmail.com>
2022-03-22 20:20:48 -0300

    back validation product form

--- public/js/productFormValidation.js
+++ public/js/productFormValidation.js
@@ -1,65 +1,65 @@
-window.addEventListener('load', function(){
-    let formulario = document.querySelector('form');
+// window.addEventListener('load', function(){
+//     let formulario = document.querySelector('form');
 
-    formulario.addEventListener('submit',(e)=>{
+//     formulario.addEventListener('submit',(e)=>{
 
-        let errores = [];
+//         let errores = [];
 
-        let fileValid = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-        let nombre = document.getElementById('name');
-        let description = document.getElementById('description');
-        let imagen = document.getElementById('image');
-        let codigo = document.getElementById('sku');
-        let precio = document.getElementById('price');
-        let descuento = document.getElementById('discount');
-        let categorias = document.getElementById('select-state');
+//         let fileValid = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+//         let nombre = document.getElementById('name');
+//         let description = document.getElementById('description');
+//         let imagen = document.getElementById('image');
+//         let codigo = document.getElementById('sku');
+//         let precio = document.getElementById('price');
+//         let descuento = document.getElementById('discount');
+//         let categorias = document.getElementById('select-state');
 
 
-        if(codigo.value == ''){
-            errores.push('El campo de código sku tiene que estar completo');
+//         if(codigo.value == ''){
+//             errores.push('El campo de código sku tiene que estar completo');
             
-        }
+//         }
 
-        if(nombre.value == ''){
-            errores.push('El campo de nombre tiene que estar completo');
+//         if(nombre.value == ''){
+//             errores.push('El campo de nombre tiene que estar completo');
             
-        } else if (nombre.value.length < 5){
-            errores.push('El campo de nombre debe tener al menos 5 caracteres');   
-        } 
+//         } else if (nombre.value.length < 5){
+//             errores.push('El campo de nombre debe tener al menos 5 caracteres');   
+//         } 
 
-        if(description.value == ''){
-            errores.push('El campo de descripcion tiene que estar completo');
-        } else if (description.value.length < 20){
-            errores.push('El campo de descripcion debe tener al menos 20 caracteres');
-        }
+//         if(description.value == ''){
+//             errores.push('El campo de descripcion tiene que estar completo');
+//         } else if (description.value.length < 20){
+//             errores.push('El campo de descripcion debe tener al menos 20 caracteres');
+//         }
 
         
-        if(precio.value == ''){
-            errores.push('El campo de precio tiene que estar completo');
-        }
+//         if(precio.value == ''){
+//             errores.push('El campo de precio tiene que estar completo');
+//         }
 
-        if(categorias.value == ''){
-            errores.push('El campo de categorias tiene que estar completo');   
-        }
+//         if(categorias.value == ''){
+//             errores.push('El campo de categorias tiene que estar completo');   
+//         }
 
-        if(imagen.value == ''){
-            errores.push('El campo de imagen tiene que estar completo');
+//         if(imagen.value == ''){
+//             errores.push('El campo de imagen tiene que estar completo');
             
-        } else if (!imagen.value.match(fileValid)){
-            errores.push('Tiene que ser en un formato compatible');   
-        }
+//         } else if (!imagen.value.match(fileValid)){
+//             errores.push('Tiene que ser en un formato compatible');   
+//         }
 
-        if(errores.length > 0){
-            e.preventDefault();
+//         if(errores.length > 0){
+//             e.preventDefault();
 
-            let ulErrores = document.querySelector('.errores');
-            ulErrores.classList.add('alert-warning');
-            for (let i = 0; i < errores.length; i++) {
-                ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
-            };
-        } else{
-            formulario.submit();
-        }
+//             let ulErrores = document.querySelector('.errores');
+//             ulErrores.classList.add('alert-warning');
+//             for (let i = 0; i < errores.length; i++) {
+//                 ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
+//             };
+//         } else{
+//             formulario.submit();
+//         }
 
-    }); 
-})
+//     }); 
+// })
--- src/middlewares/formProductValidation.js
+++ src/middlewares/formProductValidation.js
@@ -3,10 +3,12 @@ const { body } = require('express-validator');
 
 module.exports = [
     body('sku').notEmpty().withMessage('El campo no puede estar vacío'),
-    body('name').notEmpty().withMessage('El campo no puede estar vacío'),
-    body('description').notEmpty().withMessage('El campo no puede estar vacío'),
+    body('name').notEmpty().withMessage('El campo no puede estar vacío').bail()
+	.isLength({min:5}).withMessage('El nombre es demsiado corto'),
+    body('description').notEmpty().withMessage('El campo no puede estar vacío').bail()
+	.isLength({min:20}).withMessage('Debe tener al menos 20 caracteres'),
     body('price').notEmpty().withMessage('El campo no puede estar vacío'),
-    body('discount').notEmpty().withMessage('El campo no puede estar vacío'),
+    // body('discount').notEmpty().withMessage('El campo no puede estar vacío'),
     body('category').notEmpty().withMessage('El campo no puede estar vacío'),
     body('image').custom((value, {req})=>{
         let file = req.file;
